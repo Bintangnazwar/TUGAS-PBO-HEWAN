29c4d0428a801b8772b322177dfa45342a31dc29
Bintangnazwar <160728003+Bintangnazwar@users.noreply.github.com>
2024-07-03 11:46:43 +0700

    Add files via upload

---
+++ TUGAS PBO KODINGAN HEWAN/Main.java/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+      
+        String nama = "Bintang Nazwar";
+        int umur = 17;
+        String ras = "Indonesia";
+        String makanan = "ikan";
+        String warna = "Biru";
+
+        // Menampilkan informasi
+        System.out.println("Nama: " + nama);
+        System.out.println("Umur: " + umur + " tahun");
+        System.out.println("Ras: " + ras);
+        System.out.println("Makanan: " + makanan);
+        System.out.println("Warna: " + warna);
+    }
+}
